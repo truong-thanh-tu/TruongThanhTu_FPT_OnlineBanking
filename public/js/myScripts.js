6c36ce93b36407a820ec1ede57d250751f1b09e2
= <60811767+truong-thanh-tu@users.noreply.github.com>
2020-08-22 01:19:47 +0700

    Initial commit

---
+++ public/js/myScripts.js
@@ -0,0 +1,28 @@
+/**
+ Truong Thanh Tu | 20:00 21-08-2020
+ */
+
+/**
+ 1./ Account
+ a./ Login
+ */
+
+/**
+ =================================================
+ */
+
+/**
+ Login Section Begin
+ */
+
+/**
+ * Hàm đổi màu của nut đăng nhập
+ */
+ function btnLogin(){
+     document.getElementById("btn_login").style.background="#005395";
+
+ }
+
+
+
+
